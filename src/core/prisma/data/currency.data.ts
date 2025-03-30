a8869e694d8a401ba0947b0835743495159b8c93
Kennix88 <shel.live@yandex.ru>
2025-03-30 11:06:09 +0800

    Starting DB data add currency.data.ts

--- src/core/prisma/data/currency.data.ts
+++ src/core/prisma/data/currency.data.ts
@@ -2,6 +2,88 @@ import { CurrencyEnum } from '@shared/enums/currency.enum'
 
 export const CurrencyData = [
   {
-    key: CurrencyEnum,
+    key: CurrencyEnum.RUB,
+    name: 'Russian rouble',
+    symbol: '₽',
+    rate: 0,
+    coinmarketcapUCID: null,
   },
+  {
+    key: CurrencyEnum.USD,
+    name: 'United States dollar',
+    symbol: '$',
+    rate: 0,
+    coinmarketcapUCID: null,
+  },
+  {
+    key: CurrencyEnum.EUR,
+    name: 'Euro',
+    symbol: '€',
+    rate: 0,
+    coinmarketcapUCID: null,
+  },
+  {
+    key: CurrencyEnum.KZT,
+    name: 'Kazakhstani tenge',
+    symbol: '₸',
+    rate: 0,
+    coinmarketcapUCID: null,
+  },
+  {
+    key: CurrencyEnum.TON,
+    name: 'Toncoin',
+    symbol: 'TON',
+    rate: 0,
+    coinmarketcapUCID: '11419',
+  },
+  {
+    key: CurrencyEnum.MAJOR,
+    name: 'Major',
+    symbol: 'MAJOR',
+    rate: 0,
+    coinmarketcapUCID: '33188',
+  },
+  {
+    key: CurrencyEnum.NOT,
+    name: 'Notcoin',
+    symbol: 'NOT',
+    rate: 0,
+    coinmarketcapUCID: '28850',
+  },
+  {
+    key: CurrencyEnum.HMSTR,
+    name: 'Hamster Kombat',
+    symbol: 'HMSTR',
+    rate: 0,
+    coinmarketcapUCID: '32195',
+  },
+  {
+    key: CurrencyEnum.DOGS,
+    name: 'DOGS',
+    symbol: 'DOGS',
+    rate: 0,
+    coinmarketcapUCID: '32698',
+  },
+  {
+    key: CurrencyEnum.CATI,
+    name: 'Catizen',
+    symbol: 'CATI',
+    rate: 0,
+    coinmarketcapUCID: '32966',
+  },
+  {
+    key: CurrencyEnum.USDT,
+    name: 'Tether',
+    symbol: '₮',
+    rate: 0,
+    coinmarketcapUCID: '825',
+  },
+  {
+    key: CurrencyEnum.XCH,
+    name: 'Telegram Stars',
+    symbol: 'STARS',
+    rate: 0,
+    coinmarketcapUCID: null,
+  }
 ]
+
