45172d2c024e49f2262d624e829f7012aee9b9dc
Kennix88 <shel.live@yandex.ru>
2025-04-02 12:53:27 +0800

    update Prisma seeder - add Users - add UsersTelegramData - add UserBalance

--- src/core/prisma/prisma.seed.ts
+++ src/core/prisma/prisma.seed.ts
@@ -1,11 +1,13 @@
 import { AdsNetworksData } from '@core/prisma/data/ads-networks.data'
 import { CurrencyData } from '@core/prisma/data/currency.data'
 import { LanguagesData } from '@core/prisma/data/languages.data'
+import { OldUsersData } from '@core/prisma/data/old-users.data'
 import { PaymentMethodsData } from '@core/prisma/data/payment-methods.data'
 import { RolesData } from '@core/prisma/data/roles.data'
 import { SettingsData } from '@core/prisma/data/settings.data'
 import { Logger } from '@nestjs/common'
 import { PrismaClient } from '@prisma/client'
+import { UserRolesEnum } from '@shared/enums/user-roles.enum'
 
 const prisma = new PrismaClient({
   transactionOptions: {
@@ -23,42 +25,85 @@ export async function PrismaSeed() {
     skipDuplicates: true,
   })
 
-  Logger.log('Settings added successfully', 'Prisma-Seed')
+  Logger.log('Settings: added successfully', 'Prisma-Seed')
 
   await prisma.roles.createMany({
     data: RolesData,
     skipDuplicates: true,
   })
 
-  Logger.log('Roles added successfully', 'Prisma-Seed')
+  Logger.log('Roles: added successfully', 'Prisma-Seed')
 
   await prisma.language.createMany({
     data: LanguagesData,
     skipDuplicates: true,
   })
 
-  Logger.log('Languages added successfully', 'Prisma-Seed')
+  Logger.log('Languages: added successfully', 'Prisma-Seed')
 
   await prisma.currency.createMany({
     data: CurrencyData,
     skipDuplicates: true,
   })
 
-  Logger.log('Currencies added successfully', 'Prisma-Seed')
+  Logger.log('Currencies: added successfully', 'Prisma-Seed')
 
   await prisma.adsNetworks.createMany({
     data: AdsNetworksData,
     skipDuplicates: true,
   })
 
-  Logger.log('Ads networks added successfully', 'Prisma-Seed')
+  Logger.log('Ads networks: added successfully', 'Prisma-Seed')
 
   await prisma.paymentMethods.createMany({
     data: PaymentMethodsData,
     skipDuplicates: true,
   })
 
-  Logger.log('Payment methods added successfully', 'Prisma-Seed')
+  Logger.log('Payment methods: added successfully', 'Prisma-Seed')
+
+  for (const el of OldUsersData) {
+    const getUser = await prisma.users.findUnique({
+      where: {
+        telegramId: el.telegramId,
+      },
+    })
+    if (!getUser) {
+      await prisma.$transaction(async (tx) => {
+        const balance = await tx.userBalance.create({
+          data: {
+            paymentBalance: el.balance,
+          },
+        })
+        const tdata = await tx.userTelegramData.create({
+          data: {
+            firstName: 'ANONIM',
+            languageCode: 'ru',
+          },
+        })
+        const language = await tx.language.findUnique({
+          where: {
+            iso6393: el.language,
+          },
+        })
+
+        await tx.users.create({
+          data: {
+            telegramId: el.telegramId,
+            languageId: language.id,
+            balanceId: balance.id,
+            roleId: UserRolesEnum.OLD_USER,
+            telegramDataId: tdata.id,
+          },
+        })
+      })
+    }
+  }
+
+  Logger.log(
+    'Users + UsersTelegramData + UserBalance: added successfully',
+    'Prisma-Seed',
+  )
 
   Logger.log('COMPLETED', 'Prisma-Seed')
   return
