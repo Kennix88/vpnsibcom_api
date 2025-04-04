d3d6ffde1d1000910b9860a73a697096fcc7f317
Kennix88 <shel.live@yandex.ru>
2025-04-04 23:11:47 +0800

    add cache module

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -2,10 +2,15 @@ import { pinoConfig } from '@core/configs/pino.config'
 import { PrismaConnectModule } from '@core/prisma/prisma-connect.module'
 import { RedisModule } from '@core/redis/redis.module'
 import { TelegramModule } from '@integrations/telegram/telegram.module'
+import { createKeyv } from '@keyv/redis'
 import { RatesModule } from '@modules/rates/rates.module'
+import { CacheInterceptor, CacheModule } from '@nestjs/cache-manager'
 import { Module } from '@nestjs/common'
 import { ConfigModule, ConfigService } from '@nestjs/config'
+import { APP_INTERCEPTOR } from '@nestjs/core'
 import { IS_DEV_ENV } from '@shared/utils/is-dev.util'
+import { CacheableMemory } from 'cacheable'
+import Keyv from 'keyv'
 import {
   AcceptLanguageResolver,
   HeaderResolver,
@@ -26,6 +31,20 @@ import { LoggerModule } from 'nestjs-pino'
       ignoreEnvFile: !IS_DEV_ENV,
       isGlobal: true,
     }),
+    CacheModule.registerAsync({
+      isGlobal: true,
+      useFactory: async (configService: ConfigService) => ({
+        ttl: 60000,
+        stores: [
+          new Keyv({
+            store: new CacheableMemory({ ttl: 60000, lruSize: 5000 }),
+          }),
+          createKeyv(configService.getOrThrow<string>('REDIS_URL')),
+        ],
+      }),
+      inject: [ConfigService],
+    }),
+
     I18nModule.forRootAsync({
       useFactory: () => ({
         disableMiddleware: true,
@@ -50,6 +69,11 @@ import { LoggerModule } from 'nestjs-pino'
     RatesModule,
   ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: CacheInterceptor,
+    },
+  ],
 })
 export class CoreModule {}
