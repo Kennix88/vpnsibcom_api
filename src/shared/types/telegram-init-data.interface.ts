17f50dbb8d5b0f8805027eae19a12b0219c36094
Kennix88 <shel.live@yandex.ru>
2025-04-05 21:34:39 +0800

    create TelegramInitDataInterface

---
+++ src/shared/types/telegram-init-data.interface.ts
@@ -0,0 +1,33 @@
+export interface TelegramInitDataInterface {
+  auth_date: Date | number
+  can_send_after?: number
+  start_param?: 'startattach' | 'startapp'
+  chat_type?: 'group' | 'supergroup' | 'private' | 'channel' | 'sender'
+  chat_instance?: string
+  hash: string
+  query_id?: string
+  receiver?: TelegramInitDataUserInterface
+  chat?: TelegramInitDataChatInterface
+  user?: TelegramInitDataUserInterface
+}
+
+export interface TelegramInitDataUserInterface {
+  added_to_attachment_menu?: boolean
+  allows_write_to_pm?: boolean
+  is_premium?: boolean
+  first_name: string
+  id: number
+  is_bot?: boolean
+  last_name?: string
+  language_code?: string
+  photo_url?: string
+  username?: string
+}
+
+export interface TelegramInitDataChatInterface {
+  id: number
+  type: 'group' | 'supergroup' | 'channel'
+  title: string
+  photo_url?: string
+  username?: string
+}
