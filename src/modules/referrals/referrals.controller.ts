421b6a7115d99679efc66cf9987f5737b5738d13
Kennix88 <shel.live@yandex.ru>
2025-04-19 12:26:32 +0800

    create referrals controller

---
+++ src/modules/referrals/referrals.controller.ts
@@ -0,0 +1,48 @@
+import { AuthService } from '@core/auth/auth.service'
+import { CurrentUser } from '@core/auth/decorators/current-user.decorator'
+import { JwtAuthGuard } from '@core/auth/guards/jwt-auth.guard'
+import { ReferralsService } from '@modules/referrals/referrals.service'
+import { UsersService } from '@modules/users/users.service'
+import {
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Req,
+  Res,
+  UseGuards,
+} from '@nestjs/common'
+import { Throttle } from '@nestjs/throttler'
+import { JwtPayload } from '@shared/types/jwt-payload.interface'
+import { FastifyReply, FastifyRequest } from 'fastify'
+
+@Controller('referrals')
+export class ReferralsController {
+  constructor(
+    private readonly authService: AuthService,
+    private readonly userService: UsersService,
+    private readonly referralsService: ReferralsService,
+  ) {}
+
+  @Get('my')
+  @Throttle({ defaults: { limit: 5, ttl: 60 } })
+  @HttpCode(HttpStatus.OK)
+  @UseGuards(JwtAuthGuard)
+  async getMy(
+    @CurrentUser() user: JwtPayload,
+    @Req() req: FastifyRequest,
+    @Res({ passthrough: true }) res: FastifyReply,
+  ) {
+    const token = req.headers.authorization?.split(' ')[1]
+    await this.authService.updateUserActivity(token)
+    const referrals = await this.referralsService.getReferrals(user.telegramId)
+    const userData = await this.userService.getResUserByTgId(user.telegramId)
+    return {
+      data: {
+        success: true,
+        user: userData,
+        referrals,
+      },
+    }
+  }
+}
--- src/modules/referrals/referrals.module.ts
+++ src/modules/referrals/referrals.module.ts
@@ -1,4 +1,5 @@
 import { AuthModule } from '@core/auth/auth.module'
+import { ReferralsController } from '@modules/referrals/referrals.controller'
 import { ReferralsService } from '@modules/referrals/referrals.service'
 import { UsersModule } from '@modules/users/users.module'
 import { Global, Module } from '@nestjs/common'
@@ -6,7 +7,7 @@ import { Global, Module } from '@nestjs/common'
 @Global()
 @Module({
   imports: [AuthModule, UsersModule],
-  controllers: [],
+  controllers: [ReferralsController],
   providers: [ReferralsService],
   exports: [ReferralsService],
 })
