023d23e3e87f84b383ff38350df40cf734b9ec7c
Kennix88 <shel.live@yandex.ru>
2025-06-21 17:04:51 +0800

    feat(xray): add purchase invoice subscription dto for handling subscription purchases
    Add new DTO class to validate and structure subscription purchase requests, including plan details, payment method, and subscription options.

---
+++ src/modules/xray/types/purchase-invoice-subscription.dto.ts
@@ -0,0 +1,58 @@
+import { PlansEnum } from '@modules/plans/types/plans.enum'
+import { PaymentMethodEnum } from '@shared/enums/payment-method.enum'
+import { SubscriptionPeriodEnum } from '@shared/enums/subscription-period.enum'
+import {
+  IsArray,
+  IsBoolean,
+  IsEnum,
+  IsNumber,
+  IsOptional,
+} from 'class-validator'
+
+/**
+ * DTO для покупки подписки
+ */
+export class PurchaseInvoiceSubscriptionDto {
+  @IsEnum(SubscriptionPeriodEnum)
+  period: SubscriptionPeriodEnum
+
+  @IsEnum(PlansEnum)
+  planKey: PlansEnum
+
+  @IsEnum(PaymentMethodEnum)
+  method: PaymentMethodEnum
+
+  @IsNumber()
+  @IsOptional()
+  periodMultiplier?: number = 1
+
+  @IsBoolean()
+  @IsOptional()
+  isFixedPrice?: boolean = false
+
+  @IsNumber()
+  devicesCount: number
+
+  @IsBoolean()
+  @IsOptional()
+  isAllBaseServers?: boolean = false
+
+  @IsBoolean()
+  @IsOptional()
+  isAllPremiumServers?: boolean = false
+
+  @IsNumber()
+  @IsOptional()
+  trafficLimitGb?: number
+
+  @IsBoolean()
+  @IsOptional()
+  isUnlimitTraffic?: boolean = false
+
+  @IsArray()
+  servers: string[] = []
+
+  @IsBoolean()
+  @IsOptional()
+  isAutoRenewal?: boolean = true
+}
