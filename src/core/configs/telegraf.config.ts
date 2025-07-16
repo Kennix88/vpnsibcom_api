2c427eb261501918ed1b1d1a465003744ba33618
Kennix88 <shel.live@yandex.ru>
2025-07-16 21:58:19 +0800

    refactor(telegraf): extract redis url to variable for reusability

--- src/core/configs/telegraf.config.ts
+++ src/core/configs/telegraf.config.ts
@@ -1,17 +1,19 @@
 import { ConfigService } from '@nestjs/config'
-import { Redis } from '@telegraf/session/redis'
+import { Redis } from '@telegraf/session/redis' // импорт по умолчанию
 import type { TelegrafModuleOptions } from 'nestjs-telegraf'
 import { session } from 'telegraf'
 
 export function telegrafConfig(
   configService: ConfigService,
 ): TelegrafModuleOptions {
+  const redisUrl = configService.getOrThrow<string>('REDIS_URL')
+
   return {
     token: configService.getOrThrow<string>('TELEGRAM_BOT_TOKEN'),
     middlewares: [
       session({
         store: Redis({
-          url: configService.getOrThrow<string>('REDIS_URL'),
+          url: redisUrl,
         }),
       }),
     ],
