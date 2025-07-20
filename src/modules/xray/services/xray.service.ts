bea5d9f35502a0148d564ef9b3381130da101702
Kennix88 <shel.live@yandex.ru>
2025-07-20 16:51:30 +0800

    fix(xray): add xray-checker to client regex pattern

--- src/modules/xray/services/xray.service.ts
+++ src/modules/xray/services/xray.service.ts
@@ -245,7 +245,7 @@ export class XrayService {
 
       // Регулярное выражение для определения клиента
       const regexAllClients = new RegExp(
-        /^([Cc]lash-verge|[Cc]lash[-.]?[Mm]eta|[Ff][Ll][Cc]lash|[Cc]lash|[Ss]tash|[Mm]ihomo|[Ss]tash|SFA|SFI|SFM|SFT|[Hh]app|[Ss]treisand|v2box|v2ray[Nn][Gg]|v2ray[Nn]|[Kk]aring|[Hh]iddify|v2ray|[Hh]iddify[Nn]ext|[Hh]iddify|sing-box|SS|SSR|SSD|SSS|Outline|Shadowsocks|SSconf|TelegramBot|TwitterBot|NekoBox)/,
+        /^([Cc]lash-verge|[Cc]lash[-.]?[Mm]eta|[Ff][Ll][Cc]lash|[Cc]lash|[Ss]tash|[Mm]ihomo|[Ss]tash|SFA|SFI|SFM|SFT|[Hh]app|[Ss]treisand|v2box|v2ray[Nn][Gg]|v2ray[Nn]|[Kk]aring|[Hh]iddify|v2ray|[Hh]iddify[Nn]ext|[Hh]iddify|sing-box|SS|SSR|SSD|SSS|Outline|Shadowsocks|SSconf|TelegramBot|TwitterBot|NekoBox|[Xx]ray-[Cc]hecker)/,
       )
 
       // Логируем информацию о клиенте
