bc9bdcc8fc7fff0f3dcd9112d69642d554554254
Kennix88 <shel.live@yandex.ru>
2025-11-19 15:48:32 +0800

    feat(ads): add jwt authentication and ads service to ads module
    - Register JwtModule with async configuration using ConfigService
    - Add AdsService to providers and exports

--- src/modules/ads/ads.module.ts
+++ src/modules/ads/ads.module.ts
@@ -1,13 +1,26 @@
 import { TelegramModule } from '@integrations/telegram/telegram.module'
 import { UsersModule } from '@modules/users/users.module'
 import { forwardRef, Global, Module } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { JwtModule } from '@nestjs/jwt'
 import { AdsController } from './ads.controller'
+import { AdsService } from './ads.service'
 
 @Global()
 @Module({
-  imports: [UsersModule, forwardRef(() => TelegramModule)],
+  imports: [
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        secret: config.get<string>('ADS_SESSION_SECRET'),
+        signOptions: {},
+      }),
+    }),
+    UsersModule,
+    forwardRef(() => TelegramModule),
+  ],
   controllers: [AdsController],
-  providers: [],
-  exports: [],
+  providers: [AdsService],
+  exports: [AdsService],
 })
 export class AdsModule {}
