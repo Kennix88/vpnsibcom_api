c8c4abdef22ff4bcc684719c91bec2ef2c2fbda5
Kennix88 <shel.live@yandex.ru>
2026-04-05 02:10:03 +0800

    feat(taddy): Add getAd and adsImpressions methods
    Introduce new methods to the TaddyService for fetching ads and sending
    impression events.
    This includes integrating with the Taddy API for these new
    functionalities and adding appropriate logging.
    The `startEvent` method has also been updated to use `TaddyPubId` for
    consistency.

--- src/modules/ads/taddy.service.ts
+++ src/modules/ads/taddy.service.ts
@@ -1,27 +1,30 @@
-import { Injectable, Logger } from '@nestjs/common'
+import { Injectable } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 import axios from 'axios'
+import { PinoLogger } from 'nestjs-pino'
 import {
-  TaddyBaseRequestInterface,
+  TaddyGetAdRequestInterface,
+  TaddyGetAdResponseInterface,
+  TaddyPubId,
+  TaddySendAdImpressionEventRequestInterface,
   TaddyStartEventRequestInterface,
 } from './types/taddy.interface'
 
 @Injectable()
 export class TaddyService {
-  private readonly logger = new Logger(TaddyService.name)
   private pubId: string
   private baseUrl: string
 
-  constructor(private readonly configService: ConfigService) {
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly logger: PinoLogger,
+  ) {
     this.pubId = this.configService.getOrThrow<string>('TADDY_PUB_KEY')
     this.baseUrl = this.configService.getOrThrow<string>('TADDY_API_URL')
   }
 
   public startEvent(
-    data: Omit<
-      TaddyStartEventRequestInterface,
-      keyof TaddyBaseRequestInterface
-    >,
+    data: Omit<TaddyStartEventRequestInterface, keyof TaddyPubId>,
   ): void {
     const url = `${this.baseUrl}/events/start`
     axios
@@ -32,6 +35,48 @@ export class TaddyService {
       .catch((e) => {
         this.logger.error({
           msg: `Error start event`,
+          e,
+        })
+      })
+  }
+
+  public async getAd(
+    data: Omit<TaddyGetAdRequestInterface, keyof TaddyPubId>,
+  ): Promise<TaddyGetAdResponseInterface> {
+    const url = `${this.baseUrl}/ads/get`
+    const result = await axios
+      .post(url, {
+        pubId: this.pubId,
+        ...data,
+      })
+      .then((res) => res.data)
+      .catch((e) => {
+        this.logger.error({
+          msg: `Error get ad`,
+          e,
+        })
+        return null
+      })
+    this.logger.info({
+      msg: `Get ad`,
+      result,
+    })
+    return result
+  }
+
+  public async adsImpressions(
+    data: TaddySendAdImpressionEventRequestInterface,
+  ): Promise<void> {
+    const url = `${this.baseUrl}/ads/impressions
+`
+    await axios
+      .post(url, {
+        pubId: this.pubId,
+        ...data,
+      })
+      .catch((e) => {
+        this.logger.error({
+          msg: `Error ads impressions`,
           e,
         })
       })
