1e6639cddbd579597da2f968f26ed7c6897fa08d
Kennix88 <shel.live@yandex.ru>
2026-04-05 02:09:33 +0800

    feat(ads): Add Taddy and RichAds ad types
    Includes interfaces for Taddy and RichAds ad responses, and updates the
    AdsResInterface to accommodate these new ad types. This allows for the
    integration of additional ad networks.

--- src/modules/ads/types/ads-res.interface.ts
+++ src/modules/ads/types/ads-res.interface.ts
@@ -1,10 +1,14 @@
 import { AdsNetworkEnum } from './ads-network.enum'
 import { AdsPlaceEnum } from './ads-place.enum'
 import { AdsTypeEnum } from './ads-type.enum'
+import { RichAdsGetAdResponseInterface } from './richads.interface'
+import { TaddyGetAdResponseInterface } from './taddy.interface'
 
 export interface AdsResInterface {
   isNoAds: boolean
   ad?: AdsDataInterface
+  taddy?: TaddyGetAdResponseInterface
+  richAds?: RichAdsGetAdResponseInterface
 }
 
 export interface AdsDataInterface {
