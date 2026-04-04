8bdeb2e13bd33940a2ae682776460d4329499d97
Kennix88 <shel.live@yandex.ru>
2026-04-05 02:09:49 +0800

    feat(ads): Add Taddy ad types and interfaces
    Introduces new interfaces and enums for Taddy ad-related requests and
    responses.
    This includes types for:
    - Publisher IDs
    - Base request parameters
    - Starting events
    - Getting ads
    - Sending ad impressions
    - Getting ad feed
    - Sending feed impressions
    - Checking feed

--- src/modules/ads/types/taddy.interface.ts
+++ src/modules/ads/types/taddy.interface.ts
@@ -22,18 +22,113 @@ export enum TaddyGenderEnum {
   UNKNOWN = 'unknown',
 }
 
-export interface TaddyBaseRequestInterface {
+export interface TaddyPubId {
   pubId: string
+}
+
+export interface TaddyBaseRequestInterface extends TaddyPubId {
+  user: TaddyUserInterface
+  origin?: TaddyOriginEnum
 }
 
 export interface TaddyStartEventRequestInterface
   extends TaddyBaseRequestInterface {
-  user: TaddyUserInterface
-  origin?: TaddyOriginEnum
   start?: string
 }
 
 export enum TaddyOriginEnum {
   SERVER = 'server',
   WEB = 'web',
+}
+
+export enum TaddyAdFormatEnum {
+  BOT_AD = 'bot-ad',
+  APP_INTERSTITIAL = 'app-interstitial',
+  APP_TASK = 'app-task',
+}
+
+export interface TaddyGetAdRequestInterface extends TaddyBaseRequestInterface {
+  format: TaddyAdFormatEnum
+}
+
+export interface TaddySendAdImpressionEventRequestInterface {
+  id: string | string[]
+}
+
+export interface TaddyGetAdResponseInterface {
+  result?: {
+    id: string
+    title: string
+    description: string | null
+    image: string | null
+    icon: string | null
+    text: string | null
+    button: string | null
+    link: string
+  } | null
+}
+
+export enum TaddyImageFromatEnum {
+  JPG = 'jpg',
+  PNG = 'png',
+  WEBP = 'webp',
+}
+
+export interface TaddyGetFeedRequestInterface
+  extends TaddyBaseRequestInterface {
+  /**
+   * Формат изображения?
+   * Default: webp
+   */
+  imageFormat?: TaddyImageFromatEnum
+  /**
+   * Только целочисленные!
+   * Min: 1;
+   * Default: 4
+   */
+  limit?: number
+  /**
+   * Отмечать автоматически показанными?
+   * Default: false
+   */
+  autoImpressions?: boolean
+}
+
+export enum TaddyFeedTypeEnum {
+  APP = 'app',
+  BOT = 'bot',
+}
+
+export enum TaddyFeedStatusEnum {
+  NEW = 'new',
+  PENDING = 'pending',
+  COMPLETED = 'completed',
+}
+
+export interface TaddyGetFeedResponseInterface {
+  result: {
+    id: string
+    uid: string
+    title: string
+    description: string
+    image: string
+    type: TaddyFeedTypeEnum
+    price?: number | null
+    link: string
+    status: TaddyFeedStatusEnum
+    createdAt: string
+    expiresAt: string
+  }[]
+}
+
+export interface TaddySendFeedImpressionEventRequestInterface {
+  ids: string[]
+}
+
+export interface TaddyCheckFeedRequestInterface {
+  id: string
+}
+
+export interface TaddyCheckFeedResponseInterface {
+  result: boolean
 }
