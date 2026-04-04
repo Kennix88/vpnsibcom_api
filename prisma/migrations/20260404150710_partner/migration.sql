8fe40064c9f583e374e162b6c5015c20bef468b9
Kennix88 <shel.live@yandex.ru>
2026-04-05 02:06:56 +0800

    feat(settings): Add partner link fields to settings
    Introduces new fields to the settings table to store partner-related
    links. These include:
    - partner_bot_link
    - partner_mini_app_link
    - partner_site_link
    
    Each field is initialized with a default value.

---
+++ prisma/migrations/20260404150710_partner/migration.sql
@@ -0,0 +1,4 @@
+-- AlterTable
+ALTER TABLE "settings" ADD COLUMN     "partner_bot_link" TEXT DEFAULT 'https://t.me/tonplay?start=hUWcg19SE6EZ5e',
+ADD COLUMN     "partner_mini_app_link" TEXT DEFAULT 'https://t.me/tonplay/dapp?startapp=hUWcg19SE6EZ5e',
+ADD COLUMN     "partner_site_link" TEXT DEFAULT 'https://tonplay.fo/hUWcg1950zMXOm';
