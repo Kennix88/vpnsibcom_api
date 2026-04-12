ae58f8fc17248432009420fc901865b8d6710d2a
Kennix88 <shel.live@yandex.ru>
2026-04-12 16:00:20 +0800

    feat(ads): Implement fake ads sending mechanism
    Introduces a scheduled job to periodically send fake ads to users.
    This helps in simulating ad delivery and testing the ad notification
    system.
    The job fetches users in batches, shuffles them, and calls the ad
    retrieval and notification endpoints.
    Includes error handling and logging for the ad sending process.
    Uses a secure shuffle algorithm for user randomization.

--- src/modules/ads/richads.service.ts
+++ src/modules/ads/richads.service.ts
@@ -1,5 +1,8 @@
-import { Injectable } from '@nestjs/common'
+import { DefaultEnum } from '@core/prisma/generated/enums'
+import { PrismaService } from '@core/prisma/prisma.service'
+import { Injectable, OnModuleInit } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
+import { Cron } from '@nestjs/schedule'
 import axios from 'axios'
 import { PinoLogger } from 'nestjs-pino'
 import {
@@ -8,16 +11,27 @@ import {
 } from './types/richads.interface'
 
 @Injectable()
-export class RichAdsService {
+export class RichAdsService implements OnModuleInit {
   private pubId: string
   private baseUrl: string
+  private fakeInProgress = false
 
   constructor(
     private readonly configService: ConfigService,
     private readonly logger: PinoLogger,
+    private readonly prisma: PrismaService,
   ) {
     this.pubId = this.configService.getOrThrow<string>('RICHADS_PUB_KEY')
     this.baseUrl = this.configService.getOrThrow<string>('RICHADS_API_URL')
+  }
+
+  async onModuleInit() {
+    if (process.env.NODE_ENV === 'development') return
+    this.fakeAdsSend()
+  }
+
+  private async sleep(ms: number): Promise<void> {
+    await new Promise((resolve) => setTimeout(resolve, ms))
   }
 
   public async getAd(
@@ -43,6 +57,12 @@ export class RichAdsService {
         return []
       })
 
+    this.logger.info({
+      msg: 'Get ad - result',
+      resultLength: result.length,
+      result,
+    })
+
     const ad = result[0]
 
     this.logger.info({
@@ -53,5 +73,94 @@ export class RichAdsService {
     })
 
     return (ad ?? null) as RichAdsGetAdResponseInterface
+  }
+
+  public async secureShuffle(arr) {
+    const copy = [...arr]
+    for (let i = copy.length - 1; i > 0; i--) {
+      const rand = crypto.getRandomValues(new Uint32Array(1))[0]
+      const j = rand % (i + 1)
+      ;[copy[i], copy[j]] = [copy[j], copy[i]]
+    }
+    return copy
+  }
+
+  @Cron('11 * * * *')
+  public async fakeAdsSend() {
+    if (this.fakeInProgress) return
+    this.fakeInProgress = true
+
+    try {
+      const batchSize = 500 // Define your batch size here
+      let skip = 0
+      let users: any[] = [] // Initialize users as an empty array
+
+      const settings = await this.prisma.settings.findFirst({
+        where: {
+          key: DefaultEnum.DEFAULT,
+        },
+      })
+
+      if (!settings || !settings.isActiveFakeAds) return
+      const now = new Date()
+      const lastFakeAdsSend = new Date(settings.lastFakeAdsSend)
+      if (
+        lastFakeAdsSend.getTime() <
+        now.getTime() - settings.nextFakeAdsHours * 60 * 60 * 1000
+      )
+        return
+
+      // Fetch users in batches
+      do {
+        users = await this.prisma.users.findMany({
+          include: {
+            telegramData: true,
+          },
+          skip: skip,
+          take: batchSize,
+        })
+
+        const shuffledUsers = await this.secureShuffle(users)
+
+        const widgets = ['387042', null]
+        const getWidgets = widgets[Math.floor(Math.random() * widgets.length)]
+
+        for (const user of shuffledUsers) {
+          const ad = await this.getAd({
+            language_code: user.telegramData.languageCode,
+            telegram_id: user.telegramId,
+            ...(getWidgets !== undefined &&
+              getWidgets !== null && { widget_id: getWidgets }),
+            production: false,
+          })
+
+          if (ad && ad.notification_url) {
+            axios.get(ad.notification_url).catch((e) => {
+              this.logger.error({
+                msg: `Error send ad notification`,
+                e,
+              })
+            })
+          }
+          this.logger.info({
+            msg: 'Fake ads send - ad',
+            ad,
+          })
+
+          const delay = Math.random() * 1000
+
+          await this.sleep(delay)
+        }
+
+        skip += batchSize
+      } while (users.length === batchSize) // Continue as long as the last batch was full
+    } catch (e) {
+      this.logger.error({
+        msg: 'Error fake ads send',
+        e,
+      })
+    } finally {
+      this.fakeInProgress = false
+    }
   }
 }
