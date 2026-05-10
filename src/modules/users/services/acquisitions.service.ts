2c835089153ed419741c94e9e806745d16eaa55c
Kennix88 <shel.live@yandex.ru>
2026-05-10 22:57:55 +0800

    feat(acquisitions): Store first acquisition data
    Add tracking of the initial acquisition values (source, referral, start
    parameters, campaign, record and other data). The service now sets a
    `first*` field only when it is currently undefined, preserving the
    original
    value while still updating the corresponding `last*` fields on each
    call.
    This enables downstream analytics to distinguish between the first and
    most recent acquisition information.

--- src/modules/users/services/acquisitions.service.ts
+++ src/modules/users/services/acquisitions.service.ts
@@ -40,21 +40,47 @@ export class AcquisitionsService {
           id: user.acquisition.id,
         },
         data: {
+          ...(parseStartParams.params.source &&
+            !user.acquisition.firstSource && {
+              firstSource: parseStartParams.params.source,
+            }),
           ...(parseStartParams.params.source && {
             lastSource: parseStartParams.params.source,
+          }),
+          ...(referralKey && !user.acquisition.firstReferralId && {
+            firstReferralId: referralKey,
           }),
           ...(referralKey && {
             lastReferralId: referralKey,
           }),
+          ...(startParams && !user.acquisition.firstStartParams && {
+            firstStartParams: startParams,
+          }),
           ...(startParams && {
             lastStartParams: startParams,
           }),
+          ...(parseStartParams.params.compaing &&
+            !user.acquisition.firstCompaingId && {
+              firstCompaingId: parseStartParams.params.compaing,
+            }),
           ...(parseStartParams.params.compaing && {
             lastCompaingId: parseStartParams.params.compaing,
           }),
+          ...(parseStartParams.params.record &&
+            !user.acquisition.firstRecordId && {
+              firstRecordId: parseStartParams.params.record,
+            }),
           ...(parseStartParams.params.record && {
             lastRecordId: parseStartParams.params.record,
           }),
+          ...((Object.keys(parseStartParams.params).length > 0 ||
+            parseStartParams.none.length > 0) &&
+            !user.acquisition.firstOtherData && {
+              firstOtherData: JSON.stringify({
+                ...parseStartParams.params,
+                ...parseStartParams.none,
+              }),
+            }),
           ...((Object.keys(parseStartParams.params).length > 0 ||
             parseStartParams.none.length > 0) && {
             lastOtherData: JSON.stringify({
