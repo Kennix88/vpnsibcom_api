6bf61b6bb217a9a861a22d7686378b1a13be8ffb
Kennix88 <shel.live@yandex.ru>
2026-06-04 01:07:33 +0800

    update

--- src/core/auth/guards/jwt-auth.guard.ts
+++ src/core/auth/guards/jwt-auth.guard.ts
@@ -6,18 +6,30 @@ import {
   UnauthorizedException,
 } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
+import { Reflector } from '@nestjs/core'
 import { JwtService } from '@nestjs/jwt'
 import { FastifyRequest } from 'fastify'
+import { SKIP_AUTH_KEY } from '../decorators/skip-auth.decorator'
 
 @Injectable()
 export class JwtAuthGuard implements CanActivate {
   constructor(
+    private readonly reflector: Reflector,
     private readonly configService: ConfigService,
     private readonly jwtService: JwtService,
     private readonly tokenService: TokenService,
   ) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
+    const skipAuth = this.reflector.getAllAndOverride<boolean>(SKIP_AUTH_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ])
+
+    if (skipAuth) {
+      return true
+    }
+
     const request = context.switchToHttp().getRequest<FastifyRequest>()
     const token = this.extractTokenFromRequest(request)
 
@@ -26,13 +38,11 @@ export class JwtAuthGuard implements CanActivate {
     }
 
     try {
-      // Check for blacklist
       const isBlacklisted = await this.tokenService.isTokenBlacklisted(token)
       if (isBlacklisted) {
         throw new UnauthorizedException('Token revoked')
       }
 
-      // Token verification
       const payload = await this.jwtService.verifyAsync(token, {
         secret: this.configService.getOrThrow<string>('JWT_ACCESS_SECRET'),
       })
@@ -47,13 +57,11 @@ export class JwtAuthGuard implements CanActivate {
   }
 
   public extractTokenFromRequest(request: FastifyRequest): string | null {
-    // 1. Check in cookies
     const cookieToken = request.cookies?.access_token
     if (cookieToken && typeof cookieToken === 'string') {
       return cookieToken
     }
 
-    // 2. Check in Authorization header
     const authHeader = request.headers?.authorization
     if (authHeader && typeof authHeader === 'string') {
       const [type, token] = authHeader.split(' ')
