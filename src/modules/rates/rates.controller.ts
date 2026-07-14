0e15aa7f5b8e3fcae25a31897f8b950f97972fb0
Kennix88 <shel.live@yandex.ru>
2026-07-15 02:00:16 +0800

    Update rates.controller.ts

--- src/modules/rates/rates.controller.ts
+++ src/modules/rates/rates.controller.ts
@@ -1,9 +1,9 @@
+// rates.controller.ts
 import { PreventDuplicateRequest } from '@core/auth/decorators/prevent-duplicate.decorator'
 import { RatesService } from '@modules/rates/rates.service'
-import { Controller, Get, HttpCode, HttpStatus, Req, Res } from '@nestjs/common'
+import { Controller, Get, HttpCode, HttpStatus } from '@nestjs/common'
 import { Throttle } from '@nestjs/throttler'
 import { Public } from '@vpnsibcom/src/core/auth/decorators/public.decorator'
-import { FastifyReply, FastifyRequest } from 'fastify'
 
 @Controller('currency')
 export class RatesController {
@@ -14,17 +14,14 @@ export class RatesController {
   @PreventDuplicateRequest(120)
   @Throttle({ defaults: { limit: 5, ttl: 60 } })
   @HttpCode(HttpStatus.OK)
-  async get(
-    @Req() req: FastifyRequest,
-    @Res({ passthrough: true }) res: FastifyReply,
-  ) {
-    const rates = await this.ratesService.getRates()
-    const currencies = await this.ratesService.getCurrencies()
+  async get() {
+    const { currencies, rates } = await this.ratesService.getCurrencyData()
+
     return {
       data: {
         success: true,
-        currencies: currencies,
-        rates: rates,
+        currencies,
+        rates,
       },
     }
   }
