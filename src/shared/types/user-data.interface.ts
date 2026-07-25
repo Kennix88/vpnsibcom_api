c5bdf8049ede36918d072791bc4c5605a7b80160
Kennix88 <shel.live@yandex.ru>
2026-07-26 00:19:38 +0800

    Update user-data.interface.ts

--- src/shared/types/user-data.interface.ts
+++ src/shared/types/user-data.interface.ts
@@ -18,7 +18,6 @@ export interface UserDataInterface {
   photoUrl?: string
   languageCode: string
   currencyCode: CurrencyEnum
-  referralsCount: number
   isTgProgramPartner: boolean
   tgProgramPartnerExpiredAt?: Date
   balance: UserBalanceInterface
@@ -27,6 +26,10 @@ export interface UserDataInterface {
   nextAdsRewardAt?: Date
   nextAdsgramTaskAt?: Date
   minPayStars: number
+  referralReactivations: number
+  referralsCount: number
+  referralsWeekSubCount: number
+  reactivationDays: number
   lastFullscreenViewedAt?: Date
   premium: PremiumStatusInterface
 }
